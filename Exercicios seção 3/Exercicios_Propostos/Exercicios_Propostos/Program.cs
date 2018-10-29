aae2961f08f367e04458a61e69bc45cff5012d42
Kelvynator <kelvynra@gmail.com>
2018-10-29 12:07:57 -0300

    exercicios 1/3

---
+++ Exercicios seção 3/Exercicios_Propostos/Exercicios_Propostos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios_Propostos {
+    class Program {
+        static void Main(string[] args) {
+
+            /*Fazer um progrmaa para ler os valores de largura e altura de um retângulo. Em seguida, mostrar na tela o valor de 
+            sua área, perímetro e diagonal.*/
+
+            Retangulo R = new Retangulo();
+
+            Console.Write("Digite a Largura do triângulo: ");
+            R.largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Digite a Altura do triângulo: ");
+            R.altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(R);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercicios seção 3/Exercicios_Propostos/Exercicios_Propostos/Retangulo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios_Propostos {
+    class Retangulo {
+        public double largura;
+        public double altura;
+
+        public double area() {
+            return largura * altura;
+        }
+
+        public double perimetro() {
+            return 2 * (largura + altura);
+        }
+
+        public double diagonal() {
+            return Math.Sqrt((largura * largura) + (altura * altura));
+        }
+
+        public override string ToString() {
+            return "AREA = "
+                + area().ToString("F2", CultureInfo.InvariantCulture)
+                + "\nPERIMETRO = "
+                + perimetro().ToString("F2", CultureInfo.InvariantCulture)
+                + "\nDIAGONAL = "
+                + diagonal().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
