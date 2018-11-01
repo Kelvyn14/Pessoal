f6d0d598f2e379da7e7e1eacf376b4ae8ee751b5
Kelvynator <kelvynra@gmail.com>
2018-10-31 21:45:34 -0300

    exercicio proposto
    Sobrescrita, construtores e encapsulamento.

---
+++ Exercicios seção 4/Exercicios seção 4/Conta.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios_seção_4 {
+    class Conta {
+        public int numero { get; private set; }
+        public string nome { get; set; }
+
+        public double saldo { get; private set; }
+
+        public Conta(int numero, string nome, double depositoValor) {
+            this.numero = numero;
+            this.nome = nome;
+            this.saldo = depositoValor;
+        }
+
+        public Conta(int numero, string nome) {
+            this.numero = numero;
+            this.nome = nome;
+            this.saldo = 0;
+        }
+
+        public void deposito(double valor) {
+            saldo = saldo + valor;
+        }
+        public void saque(double valor) {
+            saldo = saldo - valor - 5;
+        }
+
+        public override string ToString() {
+            return "Conta "
+                + numero
+                + ", Titular: "
+                + nome
+                + ", Saldo: R$"
+                + saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Exercicios seção 4/Exercicios seção 4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios_seção_4 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Digite o número da conta: ");
+            int conta = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Digite o nome do titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char depositoInicial = char.Parse(Console.ReadLine());
+            double deposito = 0;
+
+            if (depositoInicial == 's') {
+                Console.Write("Digite o valor do depósito inicial: ");
+                deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);                                
+            }
+
+            Conta C = new Conta(conta, titular, deposito);
+            Console.WriteLine("Conta criada:\n" + C);
+
+            Console.Write("Digite um valor para depósito: ");
+            double novoDeposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            C.deposito(novoDeposito);
+            Console.WriteLine("Conta atualizada:\n" + C);
+
+            Console.Write("Digite um valor para saque: ");
+            double novoSaque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            C.saque(novoSaque);
+            Console.WriteLine("Conta atualizada:\n" + C);
+
+            Console.ReadLine();
+
+        }
+    }
+}
